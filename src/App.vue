a78c4122012ab8fad5bc54d209e9ef9211489a70
cMing <mayday404@163.com>
2019-09-24 16:37:22 +0800

    remove upload

--- src/App.vue
+++ src/App.vue
@@ -23,8 +23,8 @@
       <input v-if="newInput" type="file" @change="inputChange" v-quicklySet="{t:0, l: .25, w: .44, h: .75}" alt="添加按钮" />
       <div  @click="smartMode = !smartMode" v-quicklySet="{t:0, l: .85, w: .44, h: .75}" alt="极速"></div>
       <div @click="rotateHandler" v-quicklySet="{t:0, l: 1.45, w: .44, h: .75}" alt="旋转"></div>
-      <!-- <div @click="save" v-quicklySet="{t:0, r: 0, w: .75, h: .75}" alt="保存"></div> -->
-      <div v-quicklySet="{t:0, r: 0, w: .75, h: .75}" :style="{background: 'rgba(245, 245, 245)'}"></div>
+      <div @click="save" v-quicklySet="{t:0, r: 0, w: .75, h: .75}" alt="保存"></div>
+      <!-- <div v-quicklySet="{t:0, r: 0, w: .75, h: .75}" :style="{background: 'rgba(245, 245, 245)'}"></div> -->
       <div class="smart" v-show="smartMode" v-quicklySet="{ l: .9,w: .35, h: .65, }">
         <img  src="https://cdn.leoao.com/head-img-528/jisu_icon_123.png" alt="">
       </div>
@@ -49,7 +49,6 @@ export default {
       selectedTemplateName: '',
       selectTemplateImgObj: null,
       saveTipShow: false,
-      hasUpload: false,
       output: '',
       query: getUrlParams(),
       template: [
@@ -92,7 +91,6 @@ export default {
       // 这里判断文件重复选择的问题
       this.sourceFile = evt.target.files[0]
       this.loadFile()
-      this.hasUpload = true
       this.output = ''
       this.newInput = false
       setTimeout(() => {
@@ -115,38 +113,24 @@ export default {
       this.output = ''
       this.rotate()
     },
-    // async save () {
-    //   if (this.timer) {
-    //     return false
-    //   }
-    //   let data = this.confirm()
-    //   this.output = data.base64
-    //   this.saveTipShow = true
-    //   if (this.hasUpload) {
-    //     let res = await this.$upAdmin.upload(data.file, 'act-head-img-dianshang-828-2018-')
-    //     this.succImg = `https://cdn.leoao.com/${res.key}?imageView2/2/w/400/h/400/format/jpg`
-    //     this.shareConfig()
-    //   }
-    //   this.timer = setTimeout(() => {
-    //     this.saveTipShow = false
-    //     window.clearTimeout(this.timer)
-    //     this.timer = null
-    //   }, 3000)
-    // },
+    async save () {
+      if (this.timer) {
+        return false
+      }
+      let data = this.confirm()
+      this.output = data.base64
+      this.saveTipShow = true
+      this.timer = setTimeout(() => {
+        this.saveTipShow = false
+        window.clearTimeout(this.timer)
+        this.timer = null
+      }, 3000)
+    },
     headImgLoad () {
       this.template.forEach(v => {
         v.headImg = this.headImg
       })
       this.setRightImgInfo(this.$refs.headImg)
-    },
-    shareConfig () {
-      fitTool.shareMsg({
-        directFullShare: false,
-        title: '乐刻8.28运动装备节，等你助力！',
-        link: window.location.href,
-        desc: '上传照片，生成你的828专属头像',
-        imgUrl: this.succImg || 'https://cdn.leoao.com/head-img-528/5_%E7%94%BB%E6%9D%BF%201.png?imageView2/2/w/400/h/400/format/jpg'
-      })
     },
     headImgLoadError () {
       this.headImg = 'https://cdn.leoao.com/head-img-528/5_%E7%94%BB%E6%9D%BF%201.png'
